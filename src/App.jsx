aaff814df4569aec92d0bc72d5e897d93e2aaa8b
Anish <sharmaanish256@gmail.com>
2024-05-03 18:24:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black w-full min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-7xl font-extrabold text-purple-900">
+        Github Reimagined
+      </h1>
+    </div>
+  );
+}
+
+export default App;
